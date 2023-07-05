81942b027379302433de85774928bb1f1de1b2c0
Jesse-Calebe <62192464+Jesse-Calebe@users.noreply.github.com>
2023-07-05 16:11:19 -0300

    - Cria botão de edição na view de Parceiro  - Cria evento onEdit e cria a função configuraEdicao

--- parceiros/webapp/controller/Parceiro.controller.js
+++ parceiros/webapp/controller/Parceiro.controller.js
@@ -10,10 +10,21 @@ sap.ui.define([
 
         return Controller.extend("zapp.parceiros.controller.Parceiro", {
             onInit: function () {
+                // acessa a rota de detalhe RouteParceiro do manifest.json e chama a função rotaDetalhe para ler a URL
                 let oRouter = this.getOwnerComponent().getRouter();
                 let oRotaDesejada = oRouter.getRoute("RouteParceiro");
                 
                 oRotaDesejada.attachPatternMatched(this.rotaDetalhe, this);
+
+                //gerando um modelo para controlar a edição dos campos de input
+                let oModel = new JSONModel();
+                oModel.setProperty("/habilitado", false); //declara uma propriedade "habilitado" e marca como falsa
+                this.getView().setModel(oModel, "editavel");
+
+                oModel = new JSONModel();
+                oModel.setProperty("/botaoEdit", true);
+                this.getView().setModel(oModel, "visibilidade");
+
             },
             rotaDetalhe: function (oEvent){
                 let sCodigoParceiro = oEvent.getParameter("arguments").CodigoParceiro;
@@ -25,6 +36,24 @@ sap.ui.define([
                 });
                 
                 this.getView().bindElement(sCaminho);
+            },
+            onEditButton: function (oEvent) {
+                this._ConfiguraEdicao(true);
+            },
+            onCancelButton: function (oEvent){
+                this._ConfiguraEdicao(false);
+            },
+            _ConfiguraEdicao: function(bHabilitaEdicao){
+                let oModelEditavel = this.getView().getModel("editavel");
+                let oModelVisibilidade = this.getView().getModel("visibilidade");
+
+                oModelEditavel.setProperty("/habilitado", bHabilitaEdicao);
+
+                if (bHabilitaEdicao === true){
+                    oModelVisibilidade.setProperty("/botaoEdit", false);
+                }else{
+                    oModelVisibilidade.setProperty("/botaoEdit", true);
+                }
             }
         });
     });
